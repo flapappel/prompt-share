1cd4366eb3940fa084c02524ceb444a366560ed6
flapappel <sanderbooltink@outlook.com>
2025-03-30 14:05:09 +0200

    Fix auth types by removing PrismaAdapter

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,9 @@
-import { PrismaAdapter } from "@auth/prisma-adapter"
 import { NextAuthOptions } from "next-auth"
 import { prisma } from "./prisma"
 import CredentialsProvider from "next-auth/providers/credentials"
 import bcrypt from "bcryptjs"
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
       name: "credentials",
